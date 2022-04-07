ed37e86684b46798f3d70181fd86ec0ad660b85a
Vishwas <baghel.vishwas0@gmail.com>
2022-04-07 15:09:46 +0530

    UC1- Checking Employee Attendence

---
+++ EmployeeWageCalculation/WageCalculation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageCalculation
+{
+    public class WageComputation
+    {
+        const int IS_PRESENT = 0;
+        public void EmployeeAttendence()
+        {
+            Random random = new Random();
+            int check = random.Next(0, 2);
+            if (check == IS_PRESENT)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is absent");
+        }
+    }
+}
