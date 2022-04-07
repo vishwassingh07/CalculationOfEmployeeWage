1485172b1ad8aebe54d3f185462cb9aa2d51dca9
Vishwas <baghel.vishwas0@gmail.com>
2022-04-07 16:11:29 +0530

    UC4- Calculating Employee Wage By Switch Case

--- EmployeeWageCalculation/WageCalculation.cs
+++ EmployeeWageCalculation/WageCalculation.cs
@@ -8,8 +8,8 @@ namespace EmployeeWageCalculation
 {
     public class WageComputation
     {
-        const int IS_PRESENT = 0,WAGE_PER_HR=20,FULL_DAY_HR=8,PART_TIME_HR=4,
-            IS_FULL_DAY_PRESENT=0, IS_PART_TIME_PRESENT=1;
+        const int IS_PRESENT=1, IS_ABSENT = 0 , WAGE_PER_HR=20,FULL_DAY_HR=8,PART_TIME_HR=4,
+            IS_FULL_DAY_PRESENT=1, IS_PART_TIME_PRESENT=2;
         int empHrs, totalEmpWage;
         public void EmployeeAttendence()
         {
@@ -24,17 +24,14 @@ namespace EmployeeWageCalculation
         {
             Random random = new Random();
             int check = random.Next(0, 3);
-            if (check == IS_FULL_DAY_PRESENT)
+            switch (check)
             {
-                empHrs = FULL_DAY_HR;
-            }
-            else if (check == IS_PART_TIME_PRESENT)
-            {
-                empHrs = PART_TIME_HR;
-            }
-            else
-            {
-                empHrs = 0;
+                case IS_ABSENT: empHrs = 0;
+                    break;
+                case IS_FULL_DAY_PRESENT: empHrs = FULL_DAY_HR;
+                    break;
+                case IS_PART_TIME_PRESENT: empHrs = FULL_DAY_HR;
+                    break;
             }
             totalEmpWage = empHrs * WAGE_PER_HR;
             Console.WriteLine("Total Emp Wage for a Day :" + totalEmpWage);
