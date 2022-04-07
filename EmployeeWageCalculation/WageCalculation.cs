a3766d8e2e5c4374b5573b1aa5ad45cad3dc85cf
Vishwas <baghel.vishwas0@gmail.com>
2022-04-07 16:32:11 +0530

    UC5- Calculating Employee Wages For 20 Working Days

--- EmployeeWageCalculation/WageCalculation.cs
+++ EmployeeWageCalculation/WageCalculation.cs
@@ -9,7 +9,7 @@ namespace EmployeeWageCalculation
     public class WageComputation
     {
         const int IS_PRESENT=1, IS_ABSENT = 0 , WAGE_PER_HR=20,FULL_DAY_HR=8,PART_TIME_HR=4,
-            IS_FULL_DAY_PRESENT=1, IS_PART_TIME_PRESENT=2;
+            IS_FULL_DAY_PRESENT=1, IS_PART_TIME_PRESENT=2,WORKING_DAYS_A_MONTH=20;
         int empHrs, totalEmpWage;
         public void EmployeeAttendence()
         {
@@ -22,18 +22,26 @@ namespace EmployeeWageCalculation
         }
         public void EmpWageCalculation()
         {
-            Random random = new Random();
-            int check = random.Next(0, 3);
-            switch (check)
+            for (int i = 0; i < WORKING_DAYS_A_MONTH; i++)
             {
-                case IS_ABSENT: empHrs = 0;
-                    break;
-                case IS_FULL_DAY_PRESENT: empHrs = FULL_DAY_HR;
-                    break;
-                case IS_PART_TIME_PRESENT: empHrs = FULL_DAY_HR;
-                    break;
+
+                Random random = new Random();
+                int check = random.Next(0, 3);
+                switch (check)
+                {
+                    case IS_ABSENT:
+                        empHrs = 0;
+                        break;
+                    case IS_FULL_DAY_PRESENT:
+                        empHrs = FULL_DAY_HR;
+                        break;
+                    case IS_PART_TIME_PRESENT:
+                        empHrs = PART_TIME_HR;
+                        break;
+                }
+                totalEmpWage += empHrs * WAGE_PER_HR;
             }
-            totalEmpWage = empHrs * WAGE_PER_HR;
+
             Console.WriteLine("Total Emp Wage for a Day :" + totalEmpWage);
         }
     }
